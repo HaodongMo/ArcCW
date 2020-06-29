cf459e53a730537cdfe257961e409f41d2078932
TheOnly8Z <888888zombies@gmail.com>
2020-06-29 19:37:54 -0400

    fixed typo

--- lua/weapons/arccw_base/sh_deploy.lua
+++ lua/weapons/arccw_base/sh_deploy.lua
@@ -153,7 +153,7 @@ function SWEP:Initialize()
 
     if GetConVar("arccw_mult_defaultclip"):GetInt() < 0 then
         self.Primary.DefaultClip = self.Primary.ClipSize * 3
-        if ent.Primary.ClipSize >= 100 then
+        if self.Primary.ClipSize >= 100 then
             self.Primary.DefaultClip = self.Primary.ClipSize * 2
         end
     else
