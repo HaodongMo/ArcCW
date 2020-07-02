7094a0b9583651155637c8f0a1032541b6652e22
TheOnly8Z <888888zombies@gmail.com>
2020-07-02 14:49:36 -0400

    Fixed rifle ammo double overwriting to AlyxGun

--- lua/weapons/arccw_base/sh_ttt.lua
+++ lua/weapons/arccw_base/sh_ttt.lua
@@ -147,11 +147,11 @@ function SWEP:TTT_Init()
             end
         end
     end
-    ]]
     if ArcCW.Ammo_To_TTTAmmo[self.Primary.Ammo] then
         self.Primary.Ammo = ArcCW.Ammo_To_TTTAmmo[self.Primary.Ammo]
     end
-    --self.AmmoEnt = ArcCW.TTTAmmo_To_Ent[self.Primary.Ammo] or ""
+    self.AmmoEnt = ArcCW.TTTAmmo_To_Ent[self.Primary.Ammo] or ""
+    ]]
 
     self.Primary.ClipMax = ArcCW.TTTAmmo_To_ClipMax[self.Primary.Ammo] or self.RegularClipSize or self.Primary.ClipSize
 
