11687e7cd6a1a2a3bd71002fe34ceb9e224c8d0b
TheOnly8Z <888888zombies@gmail.com>
2020-07-03 07:09:26 -0400

    TTT Client and Server settings

--- lua/arccw/client/cl_ttt.lua
+++ lua/arccw/client/cl_ttt.lua
@@ -161,3 +161,66 @@ hook.Add("TTTPrepareRound", "ArcCW_TTT_Info", function()
         end
     end
 end)
+
+hook.Add("TTTSettingsTabs", "ArcCW_TTT", function(dtabs)
+
+    local padding = dtabs:GetPadding() * 2
+
+    local panellist = vgui.Create("DPanelList", dtabs)
+    panellist:StretchToParent(0,0,padding,0)
+    panellist:EnableVerticalScrollbar(true)
+    panellist:SetPadding(10)
+    panellist:SetSpacing(10)
+
+    local dgui = vgui.Create("DForm", panellist)
+    dgui:SetName("Client Settings")
+    dgui:CheckBox("Enable round startup info", "arccw_ttt_inforoundstart")
+    dgui:CheckBox("Enable 2D3D on ammo and weapons", "arccw_2d3d")
+    dgui:CheckBox("Enable Cheap Scopes (saves perf.)", "arccw_cheapscopes")
+    dgui:CheckBox("Enable Crosshair", "arccw_crosshair")
+    dgui:CheckBox("Toggle ADS", "arccw_toggleads")
+    dgui:CheckBox("E+RMB for UBGL", "arccw_altubglkey")
+
+    panellist:AddItem(dgui)
+
+    if LocalPlayer():IsAdmin() then
+        local dgui2 = vgui.Create("DForm", panellist)
+        dgui2:SetName("Server Settings")
+        dgui2:Help("This panel will not work for dedicated servers. Use ConVars if that is the case.")
+        dgui2:CheckBox("Auto-replace Weapons", "arccw_ttt_replace")
+        dgui2:CheckBox("Auto-replace Ammo", "arccw_ttt_replaceammo")
+        dgui2:CheckBox("Random Attachments", "arccw_ttt_atts")
+        dgui2:NumSlider("Customization Mode", "arccw_ttt_customizemode", 0, 3, 0)
+        dgui2:Help("0 - No restrictions; 1 - Restricted; 2 - Pregame only; 3 - Traitor/Detective only")
+
+        local cb = dgui2:CheckBox("Free Attachments", "arccw_attinv_free")
+        cb:SetTooltip("If enabled, players have access to all attachments.\nCustomization mode may still restrict them from using them.")
+        cb = dgui2:CheckBox("Attachment Locking", "arccw_attinv_lockmode")
+        cb:SetTooltip("If enabled, picking up one attachment unlocks it for every weapon, a-la CW2.")
+    
+        local ns = dgui2:NumSlider("Body Attachment Info", "arccw_ttt_bodyattinfo", 0, 2, 0)
+        ns:SetTooltip("If enabled, searching a body will reveal the attachments on the weapon used to kill someone.")
+        dgui2:Help("0 - Off; 1 - Detectives can see; 2 - Everyone can see")
+
+        ns = dgui2:NumSlider("Lose Attachments", "arccw_attinv_loseondie", 0, 2, 0)
+        ns:SetTooltip("If enabled, players lose attachment on death and round end.")
+        dgui2:Help("0 - Persistent; 1 - Remove on death; 2 - Drop box on death")
+
+        ns = dgui2:NumSlider("Ammo Detonation Mode", "arccw_ammo_detonationmode", -1, 2, 0)
+        ns:SetTooltip("Determines what happens if ammo boxes are destroyed.")
+        dgui2:Help("-1 - No explosion; 0 - Simple; 1 - Fragmentation; 2 - Full (chance to burn up)")
+        dgui2:CheckBox("Ammo Chain Detonation", "arccw_ammo_chaindet")
+
+        dgui2:CheckBox("Force crosshair off for everyone", "arccw_override_crosshair_off")
+        dgui2:CheckBox("True Names (requires restart)", "arccw_truenames")
+
+        ns = dgui2:NumSlider("Equipment Lifetime", "arccw_equipmenttime", 60, 600, 0)
+        ns:SetTooltip("Applies to deployable equipment like Claymores, in seconds.")
+
+
+
+        panellist:AddItem(dgui2)
+    end
+
+    dtabs:AddSheet("ArcCW", panellist, "icon16/gun.png", false, false, "ArcCW Settings")
+end)
