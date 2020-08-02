94046171ac79d235951da30da251d5109ed5dd09
Fesiug <40924091+Fesiug@users.noreply.github.com>
2020-08-01 23:40:21 -0400

    Camera movement multiplier mins n maxes
    Expanded the bounds of our imaginations or maybe just the camera movement sway multiplier

--- lua/arccw/client/cl_menus.lua
+++ lua/arccw/client/cl_menus.lua
@@ -72,7 +72,7 @@ function ArcCW_Options_Client( CPanel )
     CPanel:AddControl("Slider", {Label = "#arccw.cvar.vm_swayymult", Command = "arccw_vm_swayymult", Min = -2, Max = 2, Type = "float" })
     CPanel:AddControl("Slider", {Label = "#arccw.cvar.vm_swayzmult", Command = "arccw_vm_swayzmult", Min = -2, Max = 2, Type = "float" })
     CPanel:AddControl("Header", {Description = "#arccw.cvar.vm_viewwarn"})
-    CPanel:AddControl("Slider", {Label = "#arccw.cvar.vm_coolviewmult", Command = "arccw_vm_coolview_mult", Min = -3, Max = 3, Type = "float" })
+    CPanel:AddControl("Slider", {Label = "#arccw.cvar.vm_coolviewmult", Command = "arccw_vm_coolview_mult", Min = -10, Max = 10, Type = "float" })
 
 end
 
