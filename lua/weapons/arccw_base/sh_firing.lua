b07ffc0a49578a5016af7c07708b329ce9bce5f8
littlebabyman <reallylittlebabyman@gmail.com>
2020-08-29 15:05:43 +0300

    Update sh_firing.lua

--- lua/weapons/arccw_base/sh_firing.lua
+++ lua/weapons/arccw_base/sh_firing.lua
@@ -37,8 +37,8 @@ function SWEP:PrimaryAttack()
     end
 
     if self:GetBuff_Hook("Hook_ShouldNotFire") then return end
-
-    math.randomseed((self:EntIndex() % 30241))
+	
+	math.randomseed(self:GetOwner():GetCurrentCommand():CommandNumber() + (self:EntIndex() % 30241))
             -- yeah fight me but i can call primaryattack from anywhere now bitch
                         -- ..unless you have a better idea
 
