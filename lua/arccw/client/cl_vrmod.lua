e31931c7c8cd735593395002117ab1861f962bad
JFAexe <jfa35ru@gmail.com>
2020-08-31 01:19:30 +0300

    Refactor day 2
    cl_autostats
    cl_garbage
    cl_glint
    cl_localization
    cl_npc
    cl_singleplayer
    cl_vrmod
    sh_acwcommon

--- lua/arccw/client/cl_vrmod.lua
+++ lua/arccw/client/cl_vrmod.lua
@@ -1,10 +1,12 @@
 local function addmenu()
     if not vrmod then return end
-    vrmod.AddInGameMenuItem("ArcCW Customize", 3, 1, function()
-        local wpn = LocalPlayer():GetActiveWeapon()
-        if not IsValid(wpn) or not wpn.ArcCW then return end
 
-        wpn:ToggleCustomizeHUD(not IsValid(ArcCW.InvHUD))
+    vrmod.AddInGameMenuItem("ArcCW Customize", 3, 1, function()
+        local wep = LocalPlayer():GetActiveWeapon()
+
+        if not IsValid(wep) or not wep.ArcCW then return end
+
+        wep:ToggleCustomizeHUD(not IsValid(ArcCW.InvHUD))
     end)
 end
 
--- lua/arccw/shared/sh_acwcommon.lua
+++ lua/arccw/shared/sh_acwcommon.lua
@@ -1,14 +1,17 @@
-ArcCW.HUToM = 1 / 12 * 0.3048
-ArcCW.MOAToAcc = 10 / 180 / 60
-
 ArcCW.EnableCustomization = true
+ArcCW.PresetPath          = "arccw_presets/"
 
-ArcCW.STATE_IDLE = 0
-ArcCW.STATE_SIGHTS = 1
-ArcCW.STATE_SPRINT = 2
-ArcCW.STATE_DISABLE = 3
+ArcCW.NoDraw = true
+
+ArcCW.HUToM    = 0.0254 -- 1 / 12 * 0.3048
+ArcCW.MOAToAcc = 0.00092592592 -- 10 / 180 / 60
+
+ArcCW.STATE_IDLE      = 0
+ArcCW.STATE_SIGHTS    = 1
+ArcCW.STATE_SPRINT    = 2
+ArcCW.STATE_DISABLE   = 3
 ArcCW.STATE_CUSTOMIZE = 4
-ArcCW.STATE_BIPOD = 5
+ArcCW.STATE_BIPOD     = 5
 
 ArcCW.SCROLL_NONE = 0
 ArcCW.SCROLL_ZOOM = 1
@@ -26,59 +29,59 @@ ArcCW.ShotgunShellSoundsTable = {
 }
 
 ArcCW.ReloadTimeTable = {
-    [ACT_HL2MP_GESTURE_RELOAD_AR2] = 2,
-    [ACT_HL2MP_GESTURE_RELOAD_SMG1] = 2,
-    [ACT_HL2MP_GESTURE_RELOAD_PISTOL] = 1.5,
+    [ACT_HL2MP_GESTURE_RELOAD_AR2]      = 2,
+    [ACT_HL2MP_GESTURE_RELOAD_SMG1]     = 2,
+    [ACT_HL2MP_GESTURE_RELOAD_PISTOL]   = 1.5,
     [ACT_HL2MP_GESTURE_RELOAD_REVOLVER] = 2.5,
-    [ACT_HL2MP_GESTURE_RELOAD_SHOTGUN] = 2.5,
-    [ACT_HL2MP_GESTURE_RELOAD_DUEL] = 3.25,
+    [ACT_HL2MP_GESTURE_RELOAD_SHOTGUN]  = 2.5,
+    [ACT_HL2MP_GESTURE_RELOAD_DUEL]     = 3.25,
 }
 
 ArcCW.ReplaceWeapons = {
-    ["weapon_pistol"] = true,
-    ["weapon_smg1"] = true,
-    ["weapon_ar2"] = true,
-    ["weapon_shotgun"] = true,
-    ["weapon_357"] = true,
-    ["weapon_alyxgun"] = true,
-    ["weapon_crossbow"] = true,
-    ["weapon_rpg"] = true,
+    ["weapon_pistol"]    = true,
+    ["weapon_smg1"]      = true,
+    ["weapon_ar2"]       = true,
+    ["weapon_shotgun"]   = true,
+    ["weapon_357"]       = true,
+    ["weapon_alyxgun"]   = true,
+    ["weapon_crossbow"]  = true,
+    ["weapon_rpg"]       = true,
     ["weapon_annabelle"] = true,
 }
 
 ArcCW.PenTable = {
-   [MAT_ANTLION] = 1,
+   [MAT_ANTLION]     = 1,
    [MAT_BLOODYFLESH] = 1,
-   [MAT_CONCRETE] = 0.75,
-   [MAT_DIRT] = 0.5,
-   [MAT_EGGSHELL] = 1,
-   [MAT_FLESH] = 0.25,
-   [MAT_GRATE] = 1,
-   [MAT_ALIENFLESH] = 0.25,
-   [MAT_CLIP] = 1000,
-   [MAT_SNOW] = 0.25,
-   [MAT_PLASTIC] = 0.5,
-   [MAT_METAL] = 2,
-   [MAT_SAND] = 0.25,
-   [MAT_FOLIAGE] = 0.5,
-   [MAT_COMPUTER] = 0.25,
-   [MAT_SLOSH] = 1,
-   [MAT_TILE] = 0.5,
-   [MAT_GRASS] = 0.5,
-   [MAT_VENT] = 0.75,
-   [MAT_WOOD] = 0.5,
-   [MAT_DEFAULT] = 0.75,
-   [MAT_GLASS] = 0.1,
-   [MAT_WARPSHIELD] = 1
+   [MAT_CONCRETE]    = 0.75,
+   [MAT_DIRT]        = 0.5,
+   [MAT_EGGSHELL]    = 1,
+   [MAT_FLESH]       = 0.25,
+   [MAT_GRATE]       = 1,
+   [MAT_ALIENFLESH]  = 0.25,
+   [MAT_CLIP]        = 1000,
+   [MAT_SNOW]        = 0.25,
+   [MAT_PLASTIC]     = 0.5,
+   [MAT_METAL]       = 2,
+   [MAT_SAND]        = 0.25,
+   [MAT_FOLIAGE]     = 0.5,
+   [MAT_COMPUTER]    = 0.25,
+   [MAT_SLOSH]       = 1,
+   [MAT_TILE]        = 0.5,
+   [MAT_GRASS]       = 0.5,
+   [MAT_VENT]        = 0.75,
+   [MAT_WOOD]        = 0.5,
+   [MAT_DEFAULT]     = 0.75,
+   [MAT_GLASS]       = 0.1,
+   [MAT_WARPSHIELD]  = 1
 }
 
 ArcCW.Colors = {
-    POS = Color(25, 225, 25),
+    POS     = Color(25, 225, 25),
     MINIPOS = Color(75, 225, 75),
-    NEU = Color(225, 225, 225),
+    NEU     = Color(225, 225, 225),
     MININEG = Color(225, 75, 75),
-    NEG = Color(225, 25, 25),
-    COSM = Color(100, 100, 225)
+    NEG     = Color(225, 25, 25),
+    COSM    = Color(100, 100, 225)
 }
 
 ArcCW.LHIKBones = {
@@ -105,7 +108,3 @@ ArcCW.LHIKBones = {
     "ValveBiped.Bip01_L_Wrist",
     "ValveBiped.Bip01_L_Ulna"
 }
-
-ArcCW.NoDraw = true
-
-ArcCW.PresetPath = "arccw_presets/"
