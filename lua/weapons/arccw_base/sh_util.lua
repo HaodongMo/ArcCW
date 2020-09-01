bdd2e60a4ce9b9dbabfc2f90b7af1908c83fbd76
Haodong Mo <mohaodong@gmail.com>
2020-09-01 23:50:05 +1000

    fixed

--- lua/weapons/arccw_base/sh_util.lua
+++ lua/weapons/arccw_base/sh_util.lua
@@ -6,7 +6,7 @@ function SWEP:MyEmitSound(fsound, level, pitch, vol, chan)
     -- some retard made this "sound". Fuck you, whoever did that.
     fsound = self:GetBuff_Hook("Hook_TranslateSound", fsound) or fsound
 
-    if istable(fsound) then sound = self:TableRandom(fsound) end
+    if istable(fsound) then fsound = self:TableRandom(fsound) end
 
     self:EmitSound(fsound, level, pitch, vol, chan or CHAN_AUTO)
 end
