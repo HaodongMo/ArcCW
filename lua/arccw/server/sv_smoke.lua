9c8e7c3947a454e8735ea569ade6d58aa268dc8f
Haodong Mo <mohaodong@gmail.com>
2020-09-29 14:40:17 +1000

    Fixed smoke, added developer 1 debug overlays

--- lua/arccw/server/sv_smoke.lua
+++ lua/arccw/server/sv_smoke.lua
@@ -10,21 +10,19 @@ function ArcCW:ProcessNPCSmoke()
         if !IsValid(target) then continue end
 
         npc.ArcCW_Smoked_Time = npc.ArcCW_Smoked_Time or 0
-
-        if npc.ArcCW_Smoked then
-            if npc.ArcCW_Target == target then
-                if npc.ArcCW_Smoked_Time > CurTime() then
-                    npc:SetCondition(COND_WEAPON_SIGHT_OCCLUDED)
-                    continue
-                else
-                    npc.ArcCW_Smoked = false
+        if npc.ArcCW_Smoked_Time > CurTime() then
+            if npc.ArcCW_Smoked then
+                if npc.ArcCW_Smoked_Target == target then
+                    npc:SetSchedule(SCHED_STANDOFF)
+                    debugoverlay.Cross(npc:EyePos(), 5, 0.1, Color(50, 0, 0), true)
                 end
             end
-        elseif npc.ArcCW_Smoked_Time > CurTime() then
             continue
+        else
+            npc.ArcCW_Smoked = false
         end
 
-        local sr = 450
+        local sr = 256
         local maxs = Vector(sr, sr, sr)
         local mins = -maxs
 
@@ -32,16 +30,22 @@ function ArcCW:ProcessNPCSmoke()
         local anysmoke = false
 
         for _, i in pairs(smokes) do
-            if i.ArcCWSmoke then anysmoke = true break end
+            if i.ArcCWSmoke then
+                anysmoke = true
+                break
+            end
         end
 
         if anysmoke then
             npc.ArcCW_Smoked = true
             npc.ArcCW_Smoked_Target = target
-            npc:SetCondition(COND_WEAPON_SIGHT_OCCLUDED)
+            npc:SetSchedule(SCHED_STANDOFF)
+            debugoverlay.Line(npc:EyePos(), target:WorldSpaceCenter(), 1, Color(50, 0, 0), true)
         else
             npc.ArcCW_Smoked = false
-            npc:SetCondition(COND_WEAPON_HAS_LOS)
+            if npc:GetCurrentSchedule() == SCHED_STANDOFF then
+                npc:SetSchedule(SCHED_NONE)
+            end
         end
 
         npc.ArcCW_Smoked_Time = CurTime() + 1
