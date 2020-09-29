9139f9e2e7498ee0736c98ef317326f8cce202b1
Haodong Mo <mohaodong@gmail.com>
2020-09-29 12:12:57 +1000

    Year limit works on NPCs now

--- lua/arccw/server/sv_year.lua
+++ lua/arccw/server/sv_year.lua
@@ -1,17 +1,24 @@
 hook.Add( "PlayerGiveSWEP", "ArcCW_YearLimiter", function( ply, class, swep )
     local wep = weapons.Get(class)
-    if !wep then return end
-    if !wep.ArcCW then return end
 
-    if !GetConVar("arccw_limityear_enable"):GetBool() then return end
-
-    local year = GetConVar("arccw_limityear"):GetInt()
-
-    if !wep.Trivia_Year then return end
-    if !isnumber(wep.Trivia_Year) then return end
-
-    if wep.Trivia_Year > year then
-        ply:ChatPrint( wep.PrintName .. " is outside the year limit! (" .. wep.Trivia_Year .. " > " .. year .. ")")
+    if !ArcCW:WithinYearLimit(wep) then
+        ply:ChatPrint( wep.PrintName .. " is outside the year limit!")
         return false
     end
 end )
+
+function ArcCW:WithinYearLimit(wep)
+    if !wep then return true end
+    if !wep.ArcCW then return true end
+
+    if !GetConVar("arccw_limityear_enable"):GetBool() then return true end
+
+    local year = GetConVar("arccw_limityear"):GetInt()
+
+    if !wep.Trivia_Year then return true end
+    if !isnumber(wep.Trivia_Year) then return true end
+
+    if wep.Trivia_Year > year then return false end
+
+    return true
+end
