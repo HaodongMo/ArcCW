645eeac8c0761f78479f1a60d208011cb9bf015f
Jackarunda <jackarunda@live.com>
2020-11-29 14:56:38 -0700

    alignment, JK

--- lua/arccw/client/cl_menus.lua
+++ lua/arccw/client/cl_menus.lua
@@ -223,34 +223,34 @@ local DevPanel = {
 
 local MultsPanel = {
     { type = "h", text = "#arccw.adminonly" },
-    { type = "f", text = "#arccw.cvar.mult_damage",                 var = "arccw_mult_damage", min = 0, max = 10, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_npcdamage",             var = "arccw_mult_npcdamage", min = 0, max = 5, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_range",                 var = "arccw_mult_range", min = 0.1, max = 5, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_recoil",                 var = "arccw_mult_recoil", min = 0, max = 5, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_penetration",         var = "arccw_mult_penetration", min = 0, max = 5, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_damage",          var = "arccw_mult_damage", min = 0, max = 10, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_npcdamage",       var = "arccw_mult_npcdamage", min = 0, max = 5, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_range",           var = "arccw_mult_range", min = 0.1, max = 5, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_recoil",          var = "arccw_mult_recoil", min = 0, max = 5, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_penetration",     var = "arccw_mult_penetration", min = 0, max = 5, sv = true },
     { type = "f", text = "#arccw.cvar.mult_hipfire",         var = "arccw_mult_hipfire", min = 0, max = 3, sv = true },
     { type = "f", text = "#arccw.cvar.mult_movedisp",        var = "arccw_mult_movedisp", min = 0, max = 3, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_reloadtime",         var = "arccw_mult_reloadtime", min = 0.2, max = 3, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_sighttime",             var = "arccw_mult_sighttime", min = 0.25, max = 3, sv = true },
-    { type = "i", text = "#arccw.cvar.mult_defaultclip",         var = "arccw_mult_defaultclip", min = -1, max = 1, sv = true }, -- Fix default clip first
-    { type = "f", text = "#arccw.cvar.mult_attchance",     var = "arccw_mult_attchance", min = 0, max = 10, sv = true },
-    { type = "f", text = "#arccw.cvar.mult_heat",             var = "arccw_mult_heat", min = 0, max = 3, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_reloadtime",      var = "arccw_mult_reloadtime", min = 0.2, max = 3, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_sighttime",       var = "arccw_mult_sighttime", min = 0.25, max = 3, sv = true },
+    { type = "i", text = "#arccw.cvar.mult_defaultclip",     var = "arccw_mult_defaultclip", min = -1, max = 1, sv = true }, -- Fix default clip first
+    { type = "f", text = "#arccw.cvar.mult_attchance",       var = "arccw_mult_attchance", min = 0, max = 10, sv = true },
+    { type = "f", text = "#arccw.cvar.mult_heat",            var = "arccw_mult_heat", min = 0, max = 3, sv = true },
 }
 
 local MultPresets = {
     ["#preset.default"] = {
-        arccw_mult_damage                    = "1",
+        arccw_mult_damage                   = "1",
         arccw_mult_npcdamage                = "1",
         arccw_mult_range                    = "1",
-        arccw_mult_recoil                    = "1",
-        arccw_mult_penetration                = "1",
-        arccw_mult_hipfire                    = "1",
-        arccw_mult_movedisp                    = "1",
-        arccw_mult_reloadtime                = "1",
+        arccw_mult_recoil                   = "1",
+        arccw_mult_penetration              = "1",
+        arccw_mult_hipfire                  = "1",
+        arccw_mult_movedisp                 = "1",
+        arccw_mult_reloadtime               = "1",
         arccw_mult_sighttime                = "1",
-        arccw_mult_defaultclip                = "1",
+        arccw_mult_defaultclip              = "1",
         arccw_mult_attchance                = "1",
-        arccw_mult_heat                        = "1",
+        arccw_mult_heat                     = "1",
     }
 }
 
@@ -448,23 +448,23 @@ local CrosshairPresets = {
 
 local ViewmodelPresets = {
     ["#preset.default"] = {
-        arccw_vm_coolsway                = "1",
-        arccw_vm_coolview                = "1",
-        arccw_vm_sway_sprint            = "3",
-        arccw_vm_bob_sprint                = "3",
-        arccw_vm_right                    = "0",
-        arccw_vm_forward                = "0",
-        arccw_vm_up                        = "0",
-        arccw_vm_lookxmult                = "1",
-        arccw_vm_lookymult                = "2",
-        arccw_vm_accelmult                = "1",
-        arccw_vm_swayxmult                = "-0.1",
-        arccw_vm_swayymult                = "0.1",
-        arccw_vm_swayzmult                = "-0.3",
-        arccw_vm_swaywigglemult            = "1",
-        arccw_vm_swayspeedmult            = "1",
-        arccw_vm_swayrotatemult            = "1",
-        arccw_vm_coolview_mult            = "1",
+        arccw_vm_coolsway             = "1",
+        arccw_vm_coolview             = "1",
+        arccw_vm_sway_sprint          = "3",
+        arccw_vm_bob_sprint           = "3",
+        arccw_vm_right                = "0",
+        arccw_vm_forward              = "0",
+        arccw_vm_up                   = "0",
+        arccw_vm_lookxmult            = "1",
+        arccw_vm_lookymult            = "2",
+        arccw_vm_accelmult            = "1",
+        arccw_vm_swayxmult            = "-0.1",
+        arccw_vm_swayymult            = "0.1",
+        arccw_vm_swayzmult            = "-0.3",
+        arccw_vm_swaywigglemult       = "1",
+        arccw_vm_swayspeedmult        = "1",
+        arccw_vm_swayrotatemult       = "1",
+        arccw_vm_coolview_mult        = "1",
     }
 }
 
