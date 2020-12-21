a225003180867a15071c33e159d3b744f1eeea90
TheOnly8Z <888888zombies@gmail.com>
2020-12-22 00:34:02 +0800

    Sideways sway

--- lua/arccw/client/cl_sway.lua
+++ lua/arccw/client/cl_sway.lua
@@ -11,9 +11,14 @@ function ArcCW.Sway(cmd)
     local ang = cmd:GetViewAngles()
 
     if (wpn.Sighted or wpn:GetState() == ArcCW.STATE_SIGHTS) and !wpn.NoSway and enabled:GetBool() then
-        local sway = mult:GetFloat() * ((wpn.Sway or 0) + wpn:GetBuff_Add("Add_Sway"))
-        if sway > 0 then
-            ang.p = ang.p - math.sin(CurTime() * 1.25) * 0.004 * sway
+        local sway = mult:GetFloat() * wpn:GetBuff("Sway")
+        --sway = sway * math.Clamp(1 / (wpn:GetActiveSights().ScopeMagnification or 1), 0.1, 1)
+        if wpn:InBipod() then
+            sway = sway * ((wpn.BipodDispersion or 1) * wpn:GetBuff_Mult("Mult_BipodDispersion") or 0.1)
+        end
+        if sway > 0.05 then
+            ang.p = math.Clamp(ang.p + math.sin(CurTime() * 1.25) * FrameTime() * sway, -89, 89)
+            ang.y = ang.y + math.Rand(-1, 1) * FrameTime() * sway
             cmd:SetViewAngles(ang)
         end
     end
