3b5248e6572891d994adf49036e316fe63773dd3
Haodong Mo <mohaodong@gmail.com>
2021-02-10 13:59:47 +1000

    Default shelltime is now 0. Please do not tweak this.

--- lua/weapons/arccw_base/shared.lua
+++ lua/weapons/arccw_base/shared.lua
@@ -216,7 +216,7 @@ SWEP.ShellPhysScale = 1
 SWEP.ShellPitch = 100
 SWEP.ShellSounds = ArcCW.ShellSoundsTable
 SWEP.ShellRotate = 0
-SWEP.ShellTime = 6 -- add shell life time
+SWEP.ShellTime = 0.5
 
 SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
 SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
