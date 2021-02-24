03720b72485654a5b9fba4e63c7c351610cd2bcc
Haodong Mo <mohaodong@gmail.com>
2021-02-24 13:35:24 +1100

    The opposite of that actually

--- lua/weapons/arccw_base/cl_laser.lua
+++ lua/weapons/arccw_base/cl_laser.lua
@@ -164,13 +164,13 @@ function SWEP:DrawLaser(laser, model, color, world)
 
         SetMat(flaremat)
 
-        if world then
+        if !world then
             cam.IgnoreZ(true)
         end
         DrawSprite(laserpos, rad, rad, color)
         DrawSprite(laserpos, glr, glr, color_white)
 
-        if world then
+        if !world then
             cam.IgnoreZ(false)
         end
     end
