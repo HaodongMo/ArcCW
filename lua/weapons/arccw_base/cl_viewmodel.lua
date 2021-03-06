95934eb56d9ac7761682c1401d2af392effd8e3d
datae <datae@protonmail.com>
2021-03-06 20:59:00 +0100

    coolsway: micro-optimization

--- lua/weapons/arccw_base/cl_viewmodel.lua
+++ lua/weapons/arccw_base/cl_viewmodel.lua
@@ -327,11 +327,10 @@ function SWEP:GetViewModelPosition(pos, ang)
 
         local xangdiff = m_angdif(eyeangles.x, lasteyeangles.x) * lookxmult
         local yangdiff = m_angdif(eyeangles.y, lasteyeangles.y) * lookymult
-        local rollamount = (strafing and owner:GetVelocity():Angle().y) or eyeangles.y
+        local rollamount = (strafing and vel:Angle().y) or eyeangles.y
         local rollangdiff = math.Clamp(m_angdif(eyeangles.y, rollamount ) / 180 * pi, -7, 7)
 
         coolswaypos.x = (0.25 * velmult) * m_cos(movmtcomp) * sprintmult * swayxmult * sightmult
-        -- coolswaypos.y = -math.abs((1 * velmult) * m_cos(movmtcomp)) * swayymult * sightmult
         coolswaypos.y = -math.abs((1 * velmult) * m_cos(movmtcomp)) * swayymult * sightmult
         coolswaypos.z = -math.abs((0.25 * velmult) * m_cos(movmtcomp)) * swayzmult * sightmult
 
@@ -340,12 +339,11 @@ function SWEP:GetViewModelPosition(pos, ang)
         swayangz_lerp = f_lerp(0.025, swayangz_lerp, rollangdiff)
 
         coolswayang.x = (math.abs((0.5 * velmult) * m_sin(movmt)) + swayangx_lerp * swayxmult) + (velup * -0.01 * swayzmult)
-        -- coolswayang.y = ((0.25 * velmult) * m_cos(movmt) - swayangy_lerp + swayangz_lerp * swayymult)
         coolswayang.y = ((0.25 * velmult) * m_cos(movmt) * swayymult)
         coolswayang.z = (math.min((2.5 * velmult) * m_cos(movmt), 0) + swayangy_lerp - swayangz_lerp * swayzmult) * swayrotate
 
         target.ang = target.ang + coolswayang
-        target.pos = target.pos + coolswaypos
+        target.pos:Add(coolswaypos)
     end
 
     -- For some reason, in multiplayer the sighting speed is twice as fast
