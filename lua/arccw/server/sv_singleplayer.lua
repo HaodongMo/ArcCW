927f9498ba43032552e31ac2c42637c9d76935c7
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-03-13 16:08:01 +0200

    Tab

--- lua/arccw/server/sv_singleplayer.lua
+++ lua/arccw/server/sv_singleplayer.lua
@@ -1,14 +1,12 @@
 if game.SinglePlayer() then
-
-hook.Add("EntityTakeDamage", "ArcCW_ETD", function(npc, dmg)
-    timer.Simple(0, function()
-        if !IsValid(npc) then return end
-        if npc:Health() <= 0 then
-            net.Start("arccw_sp_health")
-            net.WriteEntity(npc)
-            net.Broadcast()
-        end
+    hook.Add("EntityTakeDamage", "ArcCW_ETD", function(npc, dmg)
+        timer.Simple(0, function()
+            if !IsValid(npc) then return end
+            if npc:Health() <= 0 then
+                net.Start("arccw_sp_health")
+                net.WriteEntity(npc)
+                net.Broadcast()
+            end
+        end)
     end)
-end)
-
 end
