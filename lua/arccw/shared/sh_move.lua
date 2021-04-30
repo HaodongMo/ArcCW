5622d87493e2b7ab18757da0df14a6bf9bd72e10
Haodong Mo <mohaodong@gmail.com>
2021-04-30 23:04:05 +1000

    Update sh_move.lua

--- lua/arccw/shared/sh_move.lua
+++ lua/arccw/shared/sh_move.lua
@@ -102,7 +102,7 @@ function ArcCW.CreateMove(cmd)
 
     -- ang2 = ang2 - (wpn.ViewPunchAngle * FrameTime() * 60)
 
-    if IsFirstTimePredicted() then
+    if game.SinglePlayer() or IsFirstTimePredicted() then
         ang2 = ang2 - (Angle(wpn.RecoilAmount, wpn.RecoilAmountSide, 0) * FrameTime() * 30)
         cmd:SetViewAngles(ang2)
     end
