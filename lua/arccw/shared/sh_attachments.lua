ac28686c81906e0f12ea5cd195c08a1202245329
Fesiug <40924091+Fesiug@users.noreply.github.com>
2021-05-27 16:25:57 -0400

    you must be the admin bitch
    not tested trolley

--- lua/arccw/shared/sh_attachments.lua
+++ lua/arccw/shared/sh_attachments.lua
@@ -54,6 +54,7 @@ function ArcCW:SlotAcceptsAtt(slot, wep, att)
     if (atttbl.NotForNPC or atttbl.NotForNPCs) and wep.Owner and wep.Owner:IsNPC() then
         return false
     end
+    if atttbl.AdminOnly and IsValid(wep:GetOwner()) and wep:GetOwner():IsPlayer() and !wep:GetOwner():IsAdmin() then return false end
 
     if wep.RejectAttachments and wep.RejectAttachments[att] then return false end
 
@@ -136,6 +137,7 @@ function ArcCW:PlayerGiveAtt(ply, att, amt)
 
     if !atttbl then print("Invalid att " .. att) return end
     if atttbl.Free then return end -- You can't give a free attachment, silly
+    if !ply:IsAdmin() and atttbl.AdminOnly then return false end
 
     if atttbl.InvAtt then att = atttbl.InvAtt end
 
