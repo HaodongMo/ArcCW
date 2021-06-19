e1b4debecf96130b6b674a7fc4cf7d1d7b817551
Fesiug <40924091+Fesiug@users.noreply.github.com>
2021-06-19 01:43:31 -0400

    i may be stupid

--- lua/weapons/arccw_base/sh_sights.lua
+++ lua/weapons/arccw_base/sh_sights.lua
@@ -386,7 +386,7 @@ function SWEP:SetupActiveSights()
                 evpos = evpos * (k.VMScale or Vector(1, 1, 1))
 
                 --if !s.IgnoreExtra then
-                if true then -- Always ignore extra. Allow the sight creator to define the distance.
+                if false then -- Always ignore extra. Allow the sight creator to define the distance.
                     evpos = evpos + Vector(0, k.ExtraSightDist or self.ExtraSightDist or 0, 0)
                 end
 
