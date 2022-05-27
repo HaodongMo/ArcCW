3028fc9a6bbdd706ff4aa50c416c8e5ed3bc3339
TheOnly8Z <888888zombies@gmail.com>
2022-05-27 03:00:47 -0500

    nil check

--- lua/weapons/arccw_base/sh_grenade.lua
+++ lua/weapons/arccw_base/sh_grenade.lua
@@ -16,7 +16,7 @@ function SWEP:PreThrow()
 
     if self:GetGrenadePrimed() then return end
 
-    if engine.ActiveGamemode() == "terrortown" and GetRoundState() == ROUND_PREP and GetConVar("ttt_no_nade_throw_during_prep"):GetBool() then
+    if engine.ActiveGamemode() == "terrortown" and GetRoundState and GetRoundState() == ROUND_PREP and GetConVar("ttt_no_nade_throw_during_prep") and GetConVar("ttt_no_nade_throw_during_prep"):GetBool() then
         return
     end
 
