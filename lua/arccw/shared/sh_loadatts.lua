fe110970d3c0992ffbfd5df1c2f8905e8c1d7a15
TheOnly8Z <888888zombies@gmail.com>
2022-07-12 19:03:40 -0500

    real

--- lua/arccw/shared/sh_loadatts.lua
+++ lua/arccw/shared/sh_loadatts.lua
@@ -95,7 +95,7 @@ local attachments_path = "arccw/shared/attachments/"
 local bulk_path = "arccw/shared/attachments_bulk/"
 
 local function ArcCW_LoadAtt(att_file)
-    att = {}
+    att = {} -- Do not bleed over attributes from previous attachments
     --shortname = string.sub(att_file, 1, -5)
     local s = string.Explode("/", att_file)
     shortname = string.sub(s[#s], 1, -5)
@@ -104,7 +104,7 @@ local function ArcCW_LoadAtt(att_file)
     AddCSLuaFile(att_file)
 
     ArcCW.LoadAttachmentType(att)
-    att = nil -- Do not bleed over attributes from previous attachments
+
 end
 
 local function ArcCW_LoadFolder(folder)
