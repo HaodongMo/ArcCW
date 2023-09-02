4592395b3b7aa0e7ee9b310aaef1c74f6951f576
TheOnly8Z <888888zombies@gmail.com>
2023-09-02 13:09:41 -0600

    missed cl ttt convars

--- lua/arccw/client/cl_ttt.lua
+++ lua/arccw/client/cl_ttt.lua
@@ -1,7 +1,7 @@
 if engine.ActiveGamemode() != "terrortown" then return end
 
-CreateClientConVar("arccw_ttt_inforoundstart", "1", true, false, "Whether to show ArcCW config every round.")
-CreateClientConVar("arccw_ttt_rolecrosshair", "1", true, false, "Whether to color your crosshair according to your role.")
+ArcCW.ConVars["ttt_inforoundstart"] = CreateClientConVar("arccw_ttt_inforoundstart", "1", true, false, "Whether to show ArcCW config every round.")
+ArcCW.ConVars["ttt_rolecrosshair"] = CreateClientConVar("arccw_ttt_rolecrosshair", "1", true, false, "Whether to color your crosshair according to your role.")
 
 ArcCW.TTT_AttInfo = ArcCW.TTT_AttInfo or {}
 
